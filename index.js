8f2de1e39e5dfeae3609d5bcccda09d40d70d6e8
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-09 16:32:41 +0200

    3.9

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 //app.use(morgan('tiny'))
