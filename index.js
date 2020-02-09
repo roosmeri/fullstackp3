62025d429258ff655f649761502617a034f96934
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-09 16:41:16 +0200

    3.9 heroku init

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.delete('/api/persons/:id', (req, resp) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
