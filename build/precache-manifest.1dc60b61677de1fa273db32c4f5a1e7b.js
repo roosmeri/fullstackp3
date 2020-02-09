339066208fa2475b100d4443f3976d44fb5c785a
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-09 17:31:18 +0200

    do not ignore build

---
+++ build/precache-manifest.1dc60b61677de1fa273db32c4f5a1e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4ef7f519f31f5d9ae793417bf68d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690a4b012b39fb9b87c4",
+    "url": "/static/css/main.82856a71.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE"
+  },
+  {
+    "revision": "690a4b012b39fb9b87c4",
+    "url": "/static/js/main.c6720fe1.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
